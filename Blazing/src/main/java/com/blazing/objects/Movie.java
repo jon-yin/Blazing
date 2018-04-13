c86414764c8bf8a0a99c00d392ae3de2cee50c30
Jonathan Yin <jonathan.yin.1@stonybrook.edu>
2018-04-12 21:43:43 -0400

    Added Full details of movie class

--- Blazing/src/main/java/com/blazing/objects/Movie.java
+++ Blazing/src/main/java/com/blazing/objects/Movie.java
@@ -1,15 +1,59 @@
 package com.blazing.objects;
 
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
 
 @Entity
-public class Movie {
+public class Movie extends Media{
 
-	@Id
 	private long id;
 	private String name;
+	private int boxOffice;
+	private int runTime;
+	private Celebrity director;
+	private Set<Celebrity> writers;
+
+	public int getBoxOffice() {
+		return boxOffice;
+	}
+
+	public void setBoxOffice(int boxOffice) {
+		this.boxOffice = boxOffice;
+	}
+
+	public int getRunTime() {
+		return runTime;
+	}
+
+	public void setRunTime(int runTime) {
+		this.runTime = runTime;
+	}
+
+	@OneToOne
+	@JoinColumn(name = "CELEBRITY_ID")
+	public Celebrity getDirector() {
+		return director;
+	}
+
+	public void setDirector(Celebrity director) {
+		this.director = director;
+	}
+
+	@ManyToMany
+	@JoinColumn(name = "CELEBRITY_ID")
+	public Set<Celebrity> getWriters() {
+		return writers;
+	}
+
+	public void setWriters(Set<Celebrity> writers) {
+		this.writers = writers;
+	}
 
 	public String getName() {
 		return name;
@@ -19,6 +63,8 @@ public class Movie {
 		this.name = name;
 	}
 
+	@Id
+	@GeneratedValue
 	public long getId() {
 		return id;
 	}
--- Blazing/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into MOVIE values(1,'Jumanji');
-insert into MOVIE values(2,'Mission Impossible');
-insert into MOVIE values(3,'Days of Our Lives');
