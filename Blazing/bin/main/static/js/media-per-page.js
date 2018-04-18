b3e41b628e4246ac4deb9bcdcf0e1fdf3e3d1afb
jackylo <jackylo@172.25.247.75>
2018-04-18 15:25:05 -0400

    linked search to server

--- Blazing/bin/main/static/js/media-per-page.js
+++ Blazing/bin/main/static/js/media-per-page.js
@@ -3,21 +3,19 @@ $(function(){
 	var count = $("#browse-list .details-link").length / pageSize;
 	for (var i = 0; i < count; i++){
 		$(".pagination").append('<li class="page-item"><a class="page-link" href="#">'+(i+1)+'</a></li>');
-        $(".pagination li").first().find("a").addClass("active")
-        showPage = function(page) {
+		$(".pagination li").first().find("a").addClass("active")
+		showPage = function(page) {
     	    $(".details-link").hide();
     	    $(".details-link").each(function(n) {
-    	        if (n >= pageSize * (page - 1) && n < pageSize * page)
-    	            $(this).show();
+    	    	if (n >= pageSize * (page - 1) && n < pageSize * page)
+    	    		$(this).show();
     	    });        
-    	}
-        
-    	showPage(1);
-
-    	$(".pagination li a").click(function() {
-    	    $(".pagination li a").removeClass("active");
-    	    $(this).addClass("active");
-    	    showPage(parseInt($(this).text()));
-    	});
+		}
+		showPage(1);
+		$(".pagination li").on("click",function() {
+			$(".pagination li").removeClass("active");
+			$(this).addClass("active");
+			showPage(parseInt($(this).text()));
+		});
 	}
 });
