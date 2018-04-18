765da603e5a856375bd9a8bbcc85f18712ddba99
jackylo <jackylo@172.25.247.75>
2018-04-18 13:08:56 -0400

    pagination in browse works now

--- Blazing/src/main/resources/static/js/media-per-page.js
+++ Blazing/src/main/resources/static/js/media-per-page.js
@@ -1,5 +1,23 @@
 $(function(){
 	var pageSize = properties["mediaPerPage"];
 	var count = $("#browse-list .details-link").length / pageSize;
-	console.log(count);
+	for (var i = 0; i < count; i++){
+		$(".pagination").append('<li class="page-item"><a class="page-link" href="#">'+(i+1)+'</a></li>');
+        $(".pagination li").first().find("a").addClass("active")
+        showPage = function(page) {
+    	    $(".details-link").hide();
+    	    $(".details-link").each(function(n) {
+    	        if (n >= pageSize * (page - 1) && n < pageSize * page)
+    	            $(this).show();
+    	    });        
+    	}
+        
+    	showPage(1);
+
+    	$(".pagination li a").click(function() {
+    	    $(".pagination li a").removeClass("active");
+    	    $(this).addClass("active");
+    	    showPage(parseInt($(this).text()));
+    	});
+	}
 });
