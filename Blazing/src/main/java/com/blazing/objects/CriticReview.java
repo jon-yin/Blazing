83f6bf24e3831ac59137f2f351ef49f46f1be775
Jonathan Yin <jonathan.yin.1@stonybrook.edu>
2018-04-22 22:31:13 -0400

    Critic Review

---
+++ Blazing/src/main/java/com/blazing/objects/CriticReview.java
@@ -0,0 +1,31 @@
+package com.blazing.objects;
+
+import javax.persistence.Entity;
+
+@Entity
+public class CriticReview extends Review {
+
+	private boolean isBlazing;
+	private String customScore;
+	private String publication;
+	
+	public boolean isBlazing() {
+		return isBlazing;
+	}
+	public void setBlazing(boolean isBlazing) {
+		this.isBlazing = isBlazing;
+	}
+	public String getCustomScore() {
+		return customScore;
+	}
+	public void setCustomScore(String customScore) {
+		this.customScore = customScore;
+	}
+	public String getPublication() {
+		return publication;
+	}
+	public void setPublication(String publication) {
+		this.publication = publication;
+	}
+	
+}
