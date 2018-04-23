0f7f9a61e323a6e6c7d82d1483f66bb491913be0
Jonathan Yin <jonathan.yin.1@stonybrook.edu>
2018-04-22 20:46:22 -0400

    Logout done

--- Blazing/src/main/java/com/blazing/controllers/LogoutController.java
+++ Blazing/src/main/java/com/blazing/controllers/LogoutController.java
@@ -2,18 +2,17 @@ package com.blazing.controllers;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping(path="/logout")
 public class LogoutController {
 
-	@RequestMapping(method=RequestMethod.POST)
-	public void logout(HttpSession session)
+	public String logout(HttpSession session)
 	{
 		session.setAttribute("currentUser", null);
+		return "home";
 	}
 	
 	
