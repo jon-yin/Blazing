e4aec0e39622feebd66542cfb646a963e9abeb34
jackylo <jackylo@172.25.244.53>
2018-04-24 21:16:08 -0400

    add new pages for critic-home

--- Blazing/bin/main/static/js/review-system.js
+++ Blazing/bin/main/static/js/review-system.js
@@ -1,10 +1,52 @@
 // review system
 function toggleWTS() {
-
+	$("#not-interested").attr("disabled", true);
+	$("#not-interested").css("background-color","#aaaaaa");
+	$("#not-interested").css('border-color','#aaaaaa');
+	$("#want-to-see").attr("disabled", true);
+	$("#want-to-see").css("background-color","#aaaaaa");
+	$("#want-to-see").css('border-color','#aaaaaa');
+	if($("#not-interested").hasClass("active")){
+		$("#not-interested").removeClass("active");
+		$("#not-interested .button-text").replaceWith('<span class="button-text"><i class="fa fa-minus"></i> Not Interested</span>');
+	}
+	if ($("#want-to-see").hasClass("active")){
+		$("#want-to-see").css('background-color','#343a40');
+		$("#want-to-see").css('border-color','#343a40');
+		$("#want-to-see .button-text").replaceWith('<span class="button-text"><i class="fa fa-plus"></i> Want To See</span>');
+	}else{
+		$("#want-to-see").css('background-color','#28a745');
+		$("#want-to-see").css('border-color','#28a745');
+		$("#want-to-see .button-text").replaceWith('<span class="button-text"><i class="fa fa-check"></i> Will See!</span>');
+	}
+	$("#want-to-see").attr("disabled",false);
+	$("#not-interested").attr("disabled",false);
+	$("#not-interested").css('background-color','#343a40');
+	$("#not-interested").css('border-color','#343a40');
 }
 
 function toggleNI() {
-
+	$("#not-interested").attr("disabled", true);
+	$("#not-interested").css("background-color","#aaaaaa");
+	$("#want-to-see").attr("disabled", true);
+	$("#want-to-see").css("background-color","#aaaaaa");
+	if($("#want-to-see").hasClass("active")){
+		$("#want-to-see").removeClass("active");
+		$("#want-to-see .button-text").replaceWith('<span class="button-text"><i class="fa fa-plus"></i> Want To See</span>');
+	}
+	if ($("#not-interested").hasClass("active")){
+		$("#not-interested").css('background-color','#343a40');
+		$("#not-interested").css('border-color','#343a40');
+		$("#not-interested .button-text").replaceWith('<span class="button-text"><i class="fa fa-minus"></i> Not Interested</span>');
+	}else{
+		$("#not-interested").css('background-color','#dc3545');
+		$("#not-interested").css('border-color','#dc3545');
+		$("#not-interested .button-text").replaceWith('<span class="button-text"><i class="fa fa-times"></i> Pass...</span>');
+	}
+	$("#want-to-see").attr("disabled",false);
+	$("#not-interested").attr("disabled",false);
+	$("#want-to-see").css('background-color','#343a40');
+	$("#want-to-see").css('border-color','#343a40');
 }
 
 function review(score, body){
@@ -20,6 +62,7 @@ function review(score, body){
 		cache : false,
 		success : function(data) {
 			console.log("success");
+			location.reload();
 		},
 		error : function(e) {
 			console.log("ERROR: ", e);
@@ -28,41 +71,8 @@ function review(score, body){
 }
 
 $(function(){
-	// you can add bool values
-	$("#want-to-see").on("click",function(){
-		if($("#not-interested").hasClass("active")){
-			$("#not-interested").removeClass("active");
-			$("#not-interested").css('background-color','#343a40');
-			$("#not-interested").css('border-color','#343a40');
-			$("#not-interested .button-text").replaceWith('<span class="button-text"><i class="fa fa-minus"></i> Not Interested</span>');
-		}
-		if ($(this).hasClass("active")){
-			$(this).css('background-color','#343a40');
-			$(this).css('border-color','#343a40');
-			$("#want-to-see .button-text").replaceWith('<span class="button-text"><i class="fa fa-plus"></i> Want To See</span>');
-		}else{
-			$(this).css('background-color','#28a745');
-			$(this).css('border-color','#28a745');
-			$("#want-to-see .button-text").replaceWith('<span class="button-text"><i class="fa fa-check"></i> Will See!</span>');
-		}
-	});
-	$("#not-interested").on("click",function(){
-		if($("#want-to-see").hasClass("active")){
-			$("#want-to-see").removeClass("active");
-			$("#want-to-see").css('background-color','#343a40');
-			$("#want-to-see").css('border-color','#343a40');
-			$("#want-to-see .button-text").replaceWith('<span class="button-text"><i class="fa fa-plus"></i> Want To See</span>');
-		}
-		if ($(this).hasClass("active")){
-			$(this).css('background-color','#343a40');
-			$(this).css('border-color','#343a40');
-			$("#not-interested .button-text").replaceWith('<span class="button-text"><i class="fa fa-minus"></i> Not Interested</span>');
-		}else{
-			$(this).css('background-color','#dc3545');
-			$(this).css('border-color','#dc3545');
-			$("#not-interested .button-text").replaceWith('<span class="button-text"><i class="fa fa-times"></i> Pass...</span>');
-		}
-	});
+	$("#want-to-see").on("click",toggleWTS);
+	$("#not-interested").on("click",toggleNI);
 	$("#submit-rating").on("click",function(e){
 		e.preventDefault();
 		var score = $("#rating-number").val();
