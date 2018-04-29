ed8e1516302c804798570cddba9771d7cce54b9f
Jonathan Yin <jonathan.yin.1@stonybrook.edu>
2018-04-28 23:17:05 -0400

    Fixed redirect.

--- Blazing/src/main/java/com/blazing/controllers/VerifyController.java
+++ Blazing/src/main/java/com/blazing/controllers/VerifyController.java
@@ -4,10 +4,10 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.blazing.objects.SuccessStatus;
 import com.blazing.services.LoginRegisterService;
@@ -20,7 +20,7 @@ public class VerifyController {
 	private LoginRegisterService service;
 
 	@RequestMapping(path="/{verification}",method=RequestMethod.GET)
-	public String verifyUser(@PathVariable("verification") String verificationString, HttpSession session, Model model)
+	public String verifyUser(@PathVariable("verification") String verificationString, HttpSession session, RedirectAttributes model)
 	{
 		SuccessStatus ss = service.verifyUser(verificationString, session);
 		if (ss.isSuccess())
@@ -28,7 +28,7 @@ public class VerifyController {
 			return "redirect:/viewprofile/" + ss.getUser().getId();
 		}
 		else{
-			model.addAttribute("verificationError", true);
+			model.addFlashAttribute("verificationError", true);
 			return "redirect:/";
 		}
 	}
