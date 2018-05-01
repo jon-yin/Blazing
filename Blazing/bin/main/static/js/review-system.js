f75d037b6fb1e58d85e98b97a33f09af900b1586
jackylo <jackylo@172.25.244.35>
2018-05-01 14:13:07 -0400

    fixing WTS and NI buttons

--- Blazing/bin/main/static/js/review-system.js
+++ Blazing/bin/main/static/js/review-system.js
@@ -1,60 +1,3 @@
-// review system
-function toggleWTS() {
-	$("#not-interested").attr("disabled", true);
-	$("#not-interested").css("background-color","#aaaaaa");
-	$("#not-interested").css('border-color','#aaaaaa');
-	$("#want-to-see").attr("disabled", true);
-	$("#want-to-see").css("background-color","#aaaaaa");
-	$("#want-to-see").css('border-color','#aaaaaa');
-	if($("#not-interested").hasClass("active")){
-		// remove from not interested
-		$("#not-interested").removeClass("active");
-		$("#not-interested .button-text").replaceWith('<span class="button-text"><i class="fa fa-minus"></i> Not Interested</span>');
-	}
-	if ($("#want-to-see").hasClass("active")){
-		// remove from want to see
-		$("#want-to-see").css('background-color','#343a40');
-		$("#want-to-see").css('border-color','#343a40');
-		$("#want-to-see .button-text").replaceWith('<span class="button-text"><i class="fa fa-plus"></i> Want To See</span>');
-	}else{
-		// add to want to see
-		$("#want-to-see").css('background-color','#28a745');
-		$("#want-to-see").css('border-color','#28a745');
-		$("#want-to-see .button-text").replaceWith('<span class="button-text"><i class="fa fa-check"></i> Will See!</span>');
-	}
-	$("#want-to-see").attr("disabled",false);
-	$("#not-interested").attr("disabled",false);
-	$("#not-interested").css('background-color','#343a40');
-	$("#not-interested").css('border-color','#343a40');
-}
-
-function toggleNI() {
-	$("#not-interested").attr("disabled", true);
-	$("#not-interested").css("background-color","#aaaaaa");
-	$("#want-to-see").attr("disabled", true);
-	$("#want-to-see").css("background-color","#aaaaaa");
-	if($("#want-to-see").hasClass("active")){
-		// remove from want to see
-		$("#want-to-see").removeClass("active");
-		$("#want-to-see .button-text").replaceWith('<span class="button-text"><i class="fa fa-plus"></i> Want To See</span>');
-	}
-	if ($("#not-interested").hasClass("active")){
-		// remove from not interested
-		$("#not-interested").css('background-color','#343a40');
-		$("#not-interested").css('border-color','#343a40');
-		$("#not-interested .button-text").replaceWith('<span class="button-text"><i class="fa fa-minus"></i> Not Interested</span>');
-	}else{
-		// add to not interested
-		$("#not-interested").css('background-color','#dc3545');
-		$("#not-interested").css('border-color','#dc3545');
-		$("#not-interested .button-text").replaceWith('<span class="button-text"><i class="fa fa-times"></i> Pass...</span>');
-	}
-	$("#want-to-see").attr("disabled",false);
-	$("#not-interested").attr("disabled",false);
-	$("#want-to-see").css('background-color','#343a40');
-	$("#want-to-see").css('border-color','#343a40');
-}
-
 function review(score, body){
 	var reviewDetails = {};
 	reviewDetails["score"] = score;
@@ -77,8 +20,6 @@ function review(score, body){
 }
 
 $(function(){
-	$("#want-to-see").on("click",toggleWTS);
-	$("#not-interested").on("click",toggleNI);
 	$("#submit-rating").on("click",function(e){
 		e.preventDefault();
 		var score = $("#rating-number").val();
