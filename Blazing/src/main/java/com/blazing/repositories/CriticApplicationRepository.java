809a43377b8ed30c8d9b1b7ba27e0f56294b005f
Jonathan Yin <jonathan.yin.1@stonybrook.edu>
2018-05-06 02:06:44 -0400

    Fixed CriticAppRepo

--- Blazing/src/main/java/com/blazing/repositories/CriticApplicationRepository.java
+++ Blazing/src/main/java/com/blazing/repositories/CriticApplicationRepository.java
@@ -3,9 +3,10 @@ package com.blazing.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.blazing.objects.CriticApplication;
+import com.blazing.objects.User;
 
 public interface CriticApplicationRepository extends JpaRepository<CriticApplication,Long> {
 	
-	CriticApplication findApplicationByUserId(long id);
+	CriticApplication findApplicationByUser(User id);
 
 }
