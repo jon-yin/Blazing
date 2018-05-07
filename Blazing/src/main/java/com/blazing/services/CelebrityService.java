e0493516aa0b22478ad37706ef6a100f39395a35
Jonathan Yin <jonathan.yin.1@stonybrook.edu>
2018-05-06 20:40:31 -0400

    Add Celebrity controller

---
+++ Blazing/src/main/java/com/blazing/services/CelebrityService.java
@@ -0,0 +1,30 @@
+package com.blazing.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.blazing.objects.Celebrity;
+import com.blazing.repositories.CelebrityRepository;
+
+public class CelebrityService {
+
+	@Autowired
+	private CelebrityRepository celebRepo;
+	
+	@Transactional
+	public Celebrity findCelebrity(long id)
+	{
+		Optional<Celebrity> celeb = celebRepo.findById(id);
+		if (celeb.isPresent())
+		{
+			return celeb.get();
+		}
+		else
+		{
+			return null;
+		}
+	}
+	
+}
