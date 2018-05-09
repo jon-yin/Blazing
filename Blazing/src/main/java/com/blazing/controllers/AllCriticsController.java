3dec06191eef97d497737e0285c44cecdb51d45d
jackylo <jackylo@172.25.244.32>
2018-05-08 22:28:28 -0400

    minor readjustments

--- Blazing/src/main/java/com/blazing/controllers/AllCriticsController.java
+++ Blazing/src/main/java/com/blazing/controllers/AllCriticsController.java
@@ -26,6 +26,7 @@ public class AllCriticsController {
 	{
 		List<User> critics = userService.getCritics();
 		model.addAttribute("critics", critics);
+		model.addAttribute("reviews", null);
 		return "critics-home";
 	}
 	
