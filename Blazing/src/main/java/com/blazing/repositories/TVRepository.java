cf3a93714d1ea2cde4c6d1f7a9337db345f727d7
Jonathan Yin <jonathan.yin.1@stonybrook.edu>
2018-05-09 02:40:45 -0400

    Added critics controller functionality

--- Blazing/src/main/java/com/blazing/repositories/TVRepository.java
+++ Blazing/src/main/java/com/blazing/repositories/TVRepository.java
@@ -11,10 +11,10 @@ import com.blazing.objects.TV;
 @Repository
 public interface TVRepository extends GeneralMediaRepository<TV>{
 
-	@Query("select t from TV t where t.blazingScore > 0.8 order by t.blazingScore DESC, size(t.reviews) DESC")
+	@Query("select t from TV t where t.blazingScore > 0.7 order by t.blazingScore DESC, size(t.reviews) DESC")
 	List<TV> findBlazingTV();
 	
-	@Query("select t from TV t where t.audienceScore > 0.8 order by t.audienceScore DESC, size(t.reviews) DESC")
+	@Query("select t from TV t where t.audienceScore > 0.7 order by t.audienceScore DESC, size(t.reviews) DESC")
 	List<TV> findPopularTV();
 
 	@CacheEvict(value = "Movies", allEntries=true)
